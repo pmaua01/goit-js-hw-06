4fbe5b58c98ecc75149fef72378d8fb47050c0d2
Mikhaylo Pobochikh <pma.ua01@gmail.com>
2022-08-08 12:58:21 +0300

    fix-06-09

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,17 @@
-console.log("Задание 6");
+console.log('Задание 6');
 
-const inputFormCheckEl = document.querySelector("#validation-input");
+const inputFormCheckEl = document.querySelector('#validation-input');
 
-inputFormCheckEl.addEventListener("blur", onBlurFocus);
+inputFormCheckEl.addEventListener('blur', onBlurFocus);
 
 function onBlurFocus(event) {
-  inputFormCheckEl.classList.add("invalid");
+  inputFormCheckEl.classList.add('invalid');
+  inputFormCheckEl.classList.remove('valid');
   if (
     event.currentTarget.value.length ===
-    Number(inputFormCheckEl.getAttribute("data-length"))
+    Number(inputFormCheckEl.getAttribute('data-length'))
   ) {
-    inputFormCheckEl.classList.remove("invalid");
-    inputFormCheckEl.classList.add("valid");
+    inputFormCheckEl.classList.remove('invalid');
+    inputFormCheckEl.classList.add('valid');
   }
 }
--- js/task-09.js
+++ js/task-09.js
@@ -1,18 +1,20 @@
-console.log("Задание 9");
+console.log('Задание 9');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const color = getRandomHexColor();
+
 const bodyChangeColorEl = document.body;
 
-const buttonChangeColorElement = document.querySelector(".change-color");
+const buttonChangeColorElement = document.querySelector('.change-color');
 
-const bodyColorEl = document.querySelector(".color");
+const bodyColorEl = document.querySelector('.color');
 
-buttonChangeColorElement.addEventListener("click", onButtonChangeColor);
+buttonChangeColorElement.addEventListener('click', onButtonChangeColor);
 
 function onButtonChangeColor(event) {
-  bodyChangeColorEl.style.backgroundColor = getRandomHexColor();
-  bodyColorEl.textContent = getRandomHexColor();
+  bodyChangeColorEl.style.backgroundColor = color;
+  bodyColorEl.textContent = color;
 }
